a253d16445b0d2b3093e198140a371338fe95200
m.morshinin <m.morshinin@b2b-center.ru>
2019-09-10 20:35:09 +0300

    Сделано задание 4

--- src/index.js
+++ src/index.js
@@ -83,7 +83,7 @@ function findAllPSiblings(where) {
 function findError(where) {
     var result = [];
 
-    for (var child of where.childNodes) {
+    for (var child of where.children) {
         result.push(child.innerText);
     }
 
