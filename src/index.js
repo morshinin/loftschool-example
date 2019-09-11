362b84d45ed66cdd0db92e4b1f5f87ad733112e6
m.morshinin <m.morshinin@b2b-center.ru>
2019-09-11 06:56:26 +0300

    Сделано задание 6

--- src/index.js
+++ src/index.js
@@ -79,6 +79,12 @@ function delegate(target, fn) {
    once(document.querySelector('button'), () => console.log('обработчик выполнился!')) // добавит такой обработчик кликов для указанного элемента, который вызовется только один раз и затем удалится
  */
 function once(target, fn) {
+    function myClick() {
+        target.removeEventListener('click', myClick);
+        fn();
+    }
+
+    target.addEventListener('click', myClick);
 }
 
 export {
